5fa9c20902a70bed3921619243e3fccf2eab946e
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-03-23 16:45:23 +0000

    commit

---
+++ src/main/java/dev/elma/services/Services.java
@@ -0,0 +1,30 @@
+package dev.elma.services;
+
+import dev.elma.stubs.Game;
+import dev.elma.stubs.gameGrpc;
+import io.grpc.stub.StreamObserver;
+
+public class Services extends gameGrpc.gameImplBase{
+
+    @Override
+    public StreamObserver<Game.guessMsg> requestReplay(StreamObserver<Game.repMsg> responseObserver) {
+        return new StreamObserver<Game.guessMsg>() {
+            @Override
+            public void onNext(Game.guessMsg guessMsg) {
+                System.out.println(guessMsg.getUsername()+" "+guessMsg.getNumber());
+                Game.repMsg repMsg = Game.repMsg.newBuilder().setContent("The number You send is").build();
+                responseObserver.onNext(repMsg);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+
+            }
+        }
+    }
+}
