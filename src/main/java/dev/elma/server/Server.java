db8520ae491c2d38ae4d23a87308be9e637abd95
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-03-23 16:54:02 +0000

    commit

--- src/main/java/dev/elma/server/Server.java
+++ src/main/java/dev/elma/server/Server.java
@@ -1,11 +1,14 @@
 package dev.elma.server;
 
+import dev.elma.services.Services;
 import io.grpc.ServerBuilder;
+import io.grpc.internal.ServerImplBuilder;
 
 import java.io.IOException;
 
 public class Server {
     public static void main(String[] args) throws Exception {
-        ServerBuilder.forPort(2001).build().start().awaitTermination();
+        ServerBuilder.forPort(2001).addService(new Services()).build().start().awaitTermination();
+
     }
 }
